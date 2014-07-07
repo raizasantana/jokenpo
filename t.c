8766c1559b6cffefea408d7f4ab7a7ea54dd2769
Raiza Santana <raiza.santanna@gmail.com>
2014-07-07 20:33:09 -0300

    Testando

---
+++ t.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+    Tesoura corta papel
+    Papel cobre pedra
+    Pedra esmaga Lagarto
+    Lagarto envenena Spock
+    Spock dobra tesoura
+    Tesoura decapita lagarto
+    Lagarto come papel
+    Papel desqualifica Spock
+    Spock vaporiza pedra
+    Pedra esmaga tesoura
+
